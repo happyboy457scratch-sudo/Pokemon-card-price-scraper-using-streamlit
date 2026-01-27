cf71685f85a523f86203048465dce18047d8258a
happyboy457scratch-sudo <happyboy457scratch@gmail.com>
2026-01-26 19:49:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,11 @@ from bs4 import BeautifulSoup
 # --- 1. INITIALIZE FIREBASE ---
 if not firebase_admin._apps:
     try:
-        firebase_info = dict(st.secrets["firebase_service_account"])
-        firebase_info["private_key"] = firebase_info["private_key"].replace('\\n', '\n')
+        firebase_info = dict(st.secrets["firebase_service_account"])firebase_info = dict(st.secrets["firebase_service_account"])
+        # This line "cleans" the key of any weird spacing issues
+        firebase_info["private_key"] = firebase_info["private_key"].replace('\\n', '\n').strip()
+        
         cred = credentials.Certificate(firebase_info)
-        firebase_admin.initialize_app(cred)
     except Exception as e:
         st.error(f"Actual Error: {e}") # This will tell us exactly what is wrong
         st.stop()
